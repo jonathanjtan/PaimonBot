1dc3747266377955fa3baf46f2908e8084514607
EC2 Default User <ec2-user@ip-172-31-0-212.us-east-2.compute.internal>
2020-10-13 07:07:40 +0000

    working initial impl

---
+++ paimon.py
@@ -0,0 +1,40 @@
+# paimon.py
+import os
+
+import discord
+from dotenv import load_dotenv
+
+# Load secret from .env
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('DISCORD_SERVER')
+ 
+client = discord.Client()
+
+# Load userdata {message.author:[characters], [weapons]}
+
+# Load "database"
+'''
+we can ignore common ascension materials because they're world drops
+characters: level ascension materials
+characters: talent ascension materials
+weapons: weapon ascension materials
+talent ascension materials: (day, location)
+weapon ascension materials: (day, location)
+'''
+
+@client.event
+async def on_ready():
+    guild = discord.utils.get(client.guilds, name=GUILD)
+    print(f'{client.user} has connected to Discord!')
+    print(f'{guild.name}(id: {guild.id})')
+
+@client.event
+async def on_message(message):
+    # prevent recursion
+    if message.author == client.user:
+        return
+
+    
+
+client.run(TOKEN)
