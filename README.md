9581b81868b45d9373c5a240a5321007266fa971
Jonathan Tan <jonathanjtan@berkeley.edu>
2020-10-13 07:32:26 +0000

    big meme hours

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # PaimonBot
 genshin impact resource tracking bot
+
+create .env and populate with DISCORD_TOKEN and DISCORD_SERVER
---
+++ database.py
@@ -0,0 +1,30 @@
+# >database.py KEKW
+from collections import defaultdict
+
+# characters to their level ascension materials
+characters_level = {
+    "amber": {"agnidus agate", "everflame seed", "small lamp grass", "arrowhead"},
+    "barbara" : {"varunada lazurite", "cleansing heart", "philanemo mushroom", "scroll"},
+    "beidou" : {"vajrada amethyst", "lightning prism", "noctilucous jade", "insignia"},
+    "bennett" : {"agnidus agate", "everflame seed", "windwheel aster", "insignia"},
+    "chongyun" : {"shivada jade", "hoarfrost core", "cor lapis", "mask"},
+    "diluc" : {"agnidus agate", "everflame seed", "small lamp grass", "insignia"},
+    "fischl" : {"vajrada amethyst", "lightning prism", "small lamp grass", "arrowhead"},
+    "jean" : {"vayuda turquoise", "hurricane seed", "dandelion seed", "mask"},
+
+    
+
+}
+
+# characters to their talent ascension materials
+characters_talent = {
+    "amber": ["freedom", "arrowhead"],
+
+}
+
+# weapons : weapon materials
+weapons_level = {}
+
+# materials : (condition, how)
+materials = {}
+
